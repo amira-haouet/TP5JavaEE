8f46e1ab902c8d80d4d4c1189d37c87e9b640c4d
Amira Haouet <73131244+htamira@users.noreply.github.com>
2021-03-17 14:31:38 +0100

    Add files via upload

---
+++ src/test/TestDao.java
@@ -0,0 +1,15 @@
+package test;
+import java.util.List;
+
+import dao.ProduitDaoImpl;
+import metier.entities.Produit;
+public class TestDao {
+public static void main(String[] args) {
+ProduitDaoImpl pdao= new ProduitDaoImpl();
+Produit prod= pdao.save(new Produit("iphone 8 plus",2800));
+System.out.println(prod);
+List<Produit> prods =pdao.produitsParMC("HP");
+for (Produit p : prods)
+System.out.println(p);
+}
+}
